6549323571c898c2038c64223d3e80f92d84ad98
morvent <rianmalikt@gmail.com>
2018-12-20 11:55:50 +0700

    This is just a temporary. I currently learning to design a web currently :)

---
+++ hello-world/rianmalik/src/pages/index.js
@@ -0,0 +1,19 @@
+import React from "react"
+
+export default () => (
+  <div style={{ margin: `4rem auto`, maxWidth: 460 }}>
+    {" "}
+    
+    <h1>Hey fellow hooman!</h1>
+    <div>
+      <p>
+        I am currently developing this site! So, comeback later! If you want to say hi, "
+        <a href="mailto:rianmalikt@gmail.com">
+           click here
+        </a>
+         " to e-mail me :)
+      </p>
+    </div>
+    <p>Posted December 20, 2018 by Rian Malik Tambengi</p>
+  </div>
+)
