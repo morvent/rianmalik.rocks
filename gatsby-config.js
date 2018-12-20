03d08ad6d29796c2aa22a81800c3b54c256c731e
morvent <rianmalikt@gmail.com>
2018-12-20 12:35:20 +0700

    Plugin correction

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,10 +1,3 @@
 module.exports = {
-    plugins: [
-        {
-            resolve: `gatsby-plugin-typography`,
-            options: {
-                pathToConfigModule: `src/utils/typography.js`,
-            },
-        },
-    ],
-}
+    plugins: [`gatsby-plugin-typography`],
+  }
